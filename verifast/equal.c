68e70156f3cfb453587dfa8d8dbf86a32698b56a
Georg Wächter <georgwaechter@googlemail.com>
2013-10-21 09:19:59 +0200

    verify equal without fixpoints, only using predicates

--- verifast/equal.c
+++ verifast/equal.c
@@ -1,21 +1,9 @@
 
-// state that equal lists of the same size are the same
-/*@ lemma_auto void eq_lists_detect(list<int> a, list<int> b);
-    requires length(a) == length(b) && eq_ints(a, b, length(a));
-    ensures a == b;
+/*@ lemma_auto void inductive_equal_lists(list<int> a, list<int> b, int n);
+    requires take(n, a) == take(n, b) && nth(n, a) == nth(n, b);
+    ensures take(n + 1, a) == take(n + 1, b);
 @*/
 
-// helper function to express that the list heads (of length n) are equal
-/*@ fixpoint bool eq_ints(list<int> a, list<int> b, int n) {
-    switch (a) {
-        case cons(a0, ax): return n <= 0 ? true : switch (b) {
-            case cons(b0, bx): return a0 == b0;
-            case nil: return false;
-        };
-        case nil: return n <= 0 ? true : false;
-    }
-}
-@*/
 
 bool equal(int *a, int* b, int size) 
 //@ requires a[0..size] |-> ?al &*& b[0..size] |-> ?bl &*& size >= 0;
@@ -25,17 +13,13 @@ bool equal(int *a, int* b, int size)
     //@ open ints(b, size, bl);
     
     for (int i = 0; i < size; i++)
-    //@ invariant i >= 0 && i <= size &*& eq_ints(al, bl, i) == true &*& ints(a, size, al) &*& ints(b, size, bl);
+    //@ invariant i >= 0 && i <= size &*& take(i, al) == take(i, bl) &*& ints(a, size, al) &*& ints(b, size, bl);
     {
     	if (a[i] != b[i])
     	{   		
     		return false;
     	}
     }
-	
-	// the lemma (see above) helps to conclude al == bl
-	// while assuming eq_ints(al, bl, size) - derived by the loop invariant
-	// TODO: is it possible to verify the algorithm without fixpoints functions and lemmas?
     
     return true;
 }
@@ -49,5 +33,10 @@ void test()
 
 	bool ret = equal(a, b, 2);
 	assert(ret == true);
+	
+	b[1] = 3;
+	
+	ret = equal(a, b, 2);
+	assert(ret == false);
 }
 
