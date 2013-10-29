de54993b751ff9226ec59f04d291195a0a27203d
Georg Wächter <georgwaechter@googlemail.com>
2013-10-29 18:48:32 +0100

    finally prove equal using a take_plus_one lemma

--- verifast/equal.c
+++ verifast/equal.c
@@ -1,4 +1,18 @@
-
+/*@ 
+// copied from listex.h
+lemma void take_plus_one<t>(int i, list<t> xs)
+    requires 0 <= i &*& i < length(xs);
+    ensures take(i + 1, xs) == append(take(i, xs), cons(nth(i, xs), nil));
+{
+    switch (xs) {
+        case nil:
+        case cons(x, xs0):
+            if (i != 0) {
+                take_plus_one(i - 1, xs0);
+            }
+    }
+}
+@*/
 
 bool equal(int *a, int* b, int size) 
 //@ requires a[0..size] |-> ?al &*& b[0..size] |-> ?bl &*& size >= 0;
@@ -6,16 +20,16 @@ bool equal(int *a, int* b, int size)
 {
     //@ open ints(a, size, al);
     //@ open ints(b, size, bl);
-    int i = 0;
-    
-    while (i < size)
-    //@ invariant a[0..i] |-> ?alx &*& b[0..i] |-> ?blx &*& alx == blx && 0 <= i &*& ints(a, size, al) &*& ints(b, size, bl);
+    for (int i = 0; i < size; i++)
+    //@ invariant 0 <= i && i <= size &*& ints(a, size, al) &*& ints(b, size, bl) &*& take(i, al) == take(i, bl);
     {
     	if (a[i] != b[i])
     	{   		
     	    return false;
     	}
     	
+    	//@ take_plus_one(i, al);
+    	//@ take_plus_one(i, bl);
     }
 
     return true;
